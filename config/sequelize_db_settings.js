f302c485a947bb56a7c7abc17620030871838b06
Wilkins Ortiz <7129104-Wilkins089@users.noreply.gitlab.com>
2021-04-25 19:31:06 -0400

    adding k8s files

--- config/sequelize_db_settings.js
+++ config/sequelize_db_settings.js
@@ -1,6 +1,6 @@
 module.exports = {
   username: process.env.DEVOPS_TEST_DATABASE_USERNAME || 'postgres',
-  password: process.env.DEVOPS_TEST_DATABASE_PASSWORD || 'D@mep3rmiso',
+  password: process.env.DEVOPS_TEST_DATABASE_PASSWORD || '',
   database: process.env.DEVOPS_TEST_DATABASE_NAME || 'postgres',
   host: process.env.DEVOPS_TEST_DATABASE_HOST || 'devops-global-postgres',
   dialect: 'postgres',
