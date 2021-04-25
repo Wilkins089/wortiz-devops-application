a83a51311744ca457d32f8b4ac7a7c38f58a7e7b
Wilkins Ortiz <7129104-Wilkins089@users.noreply.gitlab.com>
2021-04-25 19:15:41 -0400

    adding password

--- config/sequelize_db_settings.js
+++ config/sequelize_db_settings.js
@@ -1,6 +1,6 @@
 module.exports = {
   username: process.env.DEVOPS_TEST_DATABASE_USERNAME || 'postgres',
-  password: process.env.DEVOPS_TEST_DATABASE_PASSWORD || '',
+  password: process.env.DEVOPS_TEST_DATABASE_PASSWORD || 'D@mep3rmiso',
   database: process.env.DEVOPS_TEST_DATABASE_NAME || 'postgres',
   host: process.env.DEVOPS_TEST_DATABASE_HOST || 'devops-global-postgres',
   dialect: 'postgres',
